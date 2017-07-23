42eabc77925aa2b90d0c60d0f7ec8ede0e50ecc9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-07-23 19:54:03 +0100

    Tweaks

--- README.md
+++ README.md
@@ -3,21 +3,21 @@
 ![commitbit](commitbit.png)
 
 Commitbit is a microservice that grants commit access to every user who has a
-pull request merged on GitHub.
+pull request merged on GitHub. This removes manual labour on GitHub repos with
+a liberal commit bit policy.
 
-This removes manual labour on GitHub repos with a liberal commit bit policy.
-
-The rest of this document explains how to install, configure and use commitbit.
+The rest of this document explains how to install, configure and use the commitbit
+service.
 
 ## Create a personal auth token
 
 Create a [personal auth token](https://github.com/settings/tokens) with the
 following scopes:
 
-* `repo` - to comment on the PR
-* `admin:org` - to add the contributor to the team
+* `repo` - to comment on pull requests
+* `admin:org` - to add contributors to the committers team
 
-Define it in an environment variable:
+Define the token in an environment variable:
 
     export GITHUB_AUTH_TOKEN=REPLACE-WITH-YOUR-AUTH-TOKEN-HERE
 
@@ -25,17 +25,34 @@ Define it in an environment variable:
 
     export GITHUB_WEBHOOK_SECRET=$(openssl rand -base64 32)
 
-## Create a commitbit team
+## Create a committers team
 
 Create a [GitHub Team](https://help.github.com/articles/organizing-members-into-teams/)
-called `committers` (or something similar), which is the team all committers get
-added to.
+called `committers` (or something similar), which is the team new contributors will
+be added to.
 
 Define the id of the team:
 
     export GITHUB_TEAM_ID=$(./team-id REPLACE-WITH-YOUR-ORG-NAME-HERE REPLACE-WITH-YOUR-TEAM-NAME-HERE)
 
-## Deployment
+## Create a welcome message template
+
+When a contributor is added to the committers team, a comment is also made on the
+pull request welcoming the contributor. You should customise this message to your
+own project.
+
+Copy `welcome.ejs.template` to `welcome.ejs`. Change the contents of the file to
+what you want.
+
+## Deploy the service
+
+The service gets deployed to [AWS Lambda](https://aws.amazon.com/lambda).
+You need to define two environment variables:
+
+    export AWS_ACCESS_KEY_ID=REPLACE-WITH-YOUR-AWS-ACCESS-KEY-ID
+    export AWS_SECRET_ACCESS_KEY=REPLACE-WITH-YOUR-AWS-SECRET-ACCESS-KEY
+
+Now, deploy the service.
 
     ./deploy
 
@@ -51,7 +68,7 @@ Find the WebHook URL:
 
     serverless info | grep POST | tr -s ' ' | cut -d' ' -f4
 
-And remind yourself the secret:
+And remind yourself of the the secret:
 
     echo $GITHUB_WEBHOOK_SECRET
 
@@ -62,35 +79,10 @@ Finally, select only the *Pull request* event.
 ## That's it
 
 Every push to any of the repos in your GitHub organisation will now invoke the
-commitbit microservice. The service adds all committers in a push to the team you
-specified.
+commitbit microservice.
 
-This is a little overkill (it would have been sufficient to look at committers in
-merge pull requests), but the current implementation seemed like the simplest one.
-
-## Welcome message
-
-When the contributor's pull request is merged, the following message will be added
-to the pull request:
-
-> Hi @contributor,
->
-> Thanks for your contribution and welcome to the <organisation> organization's
-> <team> team. This gives you direct commit access to all repositories under
-> <org>.
-> We encourage you to create branches for new features and bug fixes, and submit
-> pull requests, optionally requesting a code review. As soon as someone from the
-> core team approves your pull request, just go ahead and merge it yourself.
->
-> We hope that this will lower the barrier to entry so that the core teams will grow
-> so we can process issues faster. Committers who contribute regularly can be
-> compensated financially, and may also be invited to join a core team.
->
-> We trust you will use this responsibly. Looking forward to your next pull request!
->
-> Aslak Hellesøy
-> Cucumber creator and project lead
->
+Whenever a pull request is merged, the user who submitted the pull request will
+be added to a team, and will receive a welcome message in the pull request.
 
 ## Credit
 
--- handler.js
+++ handler.js
@@ -1,69 +1,64 @@
-const crypto = require('crypto');
-const each = require('async/each');
-const GitHubApi = require('github');
-const github = new GitHubApi();
+const crypto = require('crypto')
+const ejs = require('ejs')
+const GitHubApi = require('github')
+const github = new GitHubApi()
 
 function signRequestBody(key, body) {
-  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
+  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`
 }
 
 module.exports.githubWebhookListener = (event, context, callback) => {
-  var errMsg; // eslint-disable-line
-  const token = process.env.GITHUB_WEBHOOK_SECRET;
-  const teamId = process.env.GITHUB_TEAM_ID;
-  const authToken = process.env.GITHUB_AUTH_TOKEN;
-  const headers = event.headers;
-  const sig = headers['X-Hub-Signature'];
-  const githubEvent = headers['X-GitHub-Event'];
-  const id = headers['X-GitHub-Delivery'];
-  const calculatedSig = signRequestBody(token, JSON.stringify(event.body));
+  var errMsg
+  const token = process.env.GITHUB_WEBHOOK_SECRET
+  const teamId = process.env.GITHUB_TEAM_ID
+  const authToken = process.env.GITHUB_AUTH_TOKEN
+  const headers = event.headers
+  const sig = headers['X-Hub-Signature']
+  const githubEvent = headers['X-GitHub-Event']
+  const id = headers['X-GitHub-Delivery']
+  const calculatedSig = signRequestBody(token, JSON.stringify(event.body))
 
   if (typeof token !== 'string') {
-    errMsg = '[401] must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
-    return callback(new Error(errMsg));
+    errMsg = '[401] must provide a \'GITHUB_WEBHOOK_SECRET\' env variable'
+    return callback(new Error(errMsg))
   }
 
   if (typeof teamId !== 'string') {
-    errMsg = '[401] must provide a \'GITHUB_TEAM_ID\' env variable';
-    return callback(new Error(errMsg));
+    errMsg = '[401] must provide a \'GITHUB_TEAM_ID\' env variable'
+    return callback(new Error(errMsg))
   }
 
   if (typeof authToken !== 'string') {
-    errMsg = '[401] must provide a \'GITHUB_AUTH_TOKEN\' env variable';
-    return callback(new Error(errMsg));
+    errMsg = '[401] must provide a \'GITHUB_AUTH_TOKEN\' env variable'
+    return callback(new Error(errMsg))
   }
 
   if (!sig) {
-    errMsg = '[401] No X-Hub-Signature found on request';
-    return callback(new Error(errMsg));
+    errMsg = '[401] No X-Hub-Signature found on request'
+    return callback(new Error(errMsg))
   }
 
   if (!githubEvent) {
-    errMsg = '[422] No X-Github-Event found on request';
-    return callback(new Error(errMsg));
+    errMsg = '[422] No X-Github-Event found on request'
+    return callback(new Error(errMsg))
   }
 
   if (githubEvent !== 'pull_request') {
-    errMsg = '[422] X-Github-Event was "' + githubEvent + '", but expected "pull_request"';
-    return callback(new Error(errMsg));
+    errMsg = '[422] X-Github-Event was "' + githubEvent + '", but expected "pull_request"'
+    return callback(new Error(errMsg))
   }
 
   if (!id) {
-    errMsg = '[401] No X-Github-Delivery found on request';
-    return callback(new Error(errMsg));
+    errMsg = '[401] No X-Github-Delivery found on request'
+    return callback(new Error(errMsg))
   }
 
   if (sig !== calculatedSig) {
-    errMsg = '[401] X-Hub-Signature incorrect. Github webhook token doesn\'t match';
-    return callback(new Error(errMsg));
+    errMsg = '[401] X-Hub-Signature incorrect. Github webhook token doesn\'t match'
+    return callback(new Error(errMsg))
   }
 
   try {
-    github.authenticate({
-      type: "token",
-      token: authToken
-    })
-
     const prEvent = event.body
     const pr = prEvent.pull_request
     const user = pr.user.login
@@ -71,44 +66,39 @@ module.exports.githubWebhookListener = (event, context, callback) => {
     const repo = prEvent.repository.name
     const number = prEvent.number
 
-    const reaction = {
-      owner: owner,
-      repo: repo,
-      number: number,
-      content: 'heart'
-    }
-    const comment = {
-      owner: owner,
-      repo: repo,
-      number: number,
-      body: `Hi @${user},
-Thanks for your contribution and welcome to the committers team!
-You can now push directly to this repo and all other repos under the cucumber
-organization. Please read up on the general contributing guidelines to learn more.
-
-Cheers,
-Aslak Hellesøy
-Creator of Cucumber
-`
-    }
-
     if (pr.state == "closed" && pr.merged) {
-      github.orgs.getTeamMembership({id: teamId, username: user}, function (err, isMember) {
+      ejs.renderFile(__dirname + '/welcome.ejs', {user, owner, repo}, function(err, welcome) {
         if(err) return callback(new Error("[500] " + err.message))
-        if(isMember) return callback(null, {
-          statusCode: 200,
-          body: `${user} is already a member`
+
+        const comment = {
+          owner: owner,
+          repo: repo,
+          number: number,
+          body: welcome
+        }
+
+        github.authenticate({
+          type: "token",
+          token: authToken
         })
 
-        github.orgs.addTeamMembership({id: teamId, username: user}, function (err) {
+        github.orgs.getTeamMembership({id: teamId, username: user}, function (err, isMember) {
           if(err) return callback(new Error("[500] " + err.message))
+          if(isMember) return callback(null, {
+            statusCode: 200,
+            body: `${user} is already a member`
+          })
 
-          github.issues.createComment(comment, function (err) {
+          github.orgs.addTeamMembership({id: teamId, username: user}, function (err) {
             if(err) return callback(new Error("[500] " + err.message))
 
-            callback(null, {
-              statusCode: 200,
-              body: `Commented on ${pr.url}`,
+            github.issues.createComment(comment, function (err) {
+              if(err) return callback(new Error("[500] " + err.message))
+
+              callback(null, {
+                statusCode: 200,
+                body: `Commented on ${pr.url}`,
+              })
             })
           })
         })
@@ -119,9 +109,8 @@ Creator of Cucumber
         body: `Nothing to do`,
       })
     }
-
   } catch(err) {
     err.message = `[500] ${err.message}`
     callback(err)
   }
-};
+}
